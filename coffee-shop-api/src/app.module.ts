e05e847b6dfccb6b6fcaa233df0faf783d74589d
Tien Nguyen <tien.nguyen@asnet.com.vn>
2026-08-24 14:17:23 +0700

    feat: config MikroORM PostgreSQL connection

--- coffee-shop-api/src/app.module.ts
+++ coffee-shop-api/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import mikroOrmConfig from './configs/mikro-orm.config';
+import { validate } from './configs/env.validation';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, validate }),
+    MikroOrmModule.forRoot(mikroOrmConfig),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- coffee-shop-api/src/main.ts
+++ coffee-shop-api/src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DEFAULT_PORT } from './common/constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? DEFAULT_PORT);
 }
 bootstrap();
